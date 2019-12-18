3914bb90b711833c7411e72b0d77ade47f776340
harshagen <harsha@genesiis.com>
2019-12-18 11:57:49 +0530

    create payrall module and component

---
+++ src/app/modules/payroll/payroll.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PayrollComponent } from './components/payroll.component';
+
+
+
+@NgModule({
+  declarations: [PayrollComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PayrollModule { }
